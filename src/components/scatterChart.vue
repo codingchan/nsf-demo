a216573d4ac6c684bafb9f4d6365634099e29159
codingchan <codingchan@gmail.com>
2022-03-24 14:52:12 +0800

    update numberRaw data for scatterChart
    Signed-off-by: codingchan <codingchan@gmail.com>

--- src/components/scatterChart.vue
+++ src/components/scatterChart.vue
@@ -30,7 +30,7 @@ function getBeforeDate (n: number) { // 获取今天前第n天的日期
   return (month < 10 ? ('0' + month) : month) + '-' + (day < 10 ? ('0' + day) : day)
 }
 
-const updateFrequency: number = 6000
+const updateFrequency: number = 3000
 const colors: string[] = ['#ead7bb', '#5d7599', '#abb6c8', '#9fcaa3', '#e78764', '#e1d839']
 
 // 原始数据
@@ -38,10 +38,10 @@ const numberRaw: Array<number[]> = [
   [0, 4, 6, 2, 1, 4],
   [4, 6, 7, 6, 3, 5],
   [7, 13, 16, 6, 7, 10],
-  [7, 15, 20, 12, 10, 12],
-  [10, 18, 22, 18, 18, 12],
-  [15, 22, 25, 22, 25, 17],
-  [20, 22, 28, 26, 30, 27]
+  [7 * 2, 15 * 2, 20 * 2, 12 * 2, 10 * 2, 12 * 2],
+  [10 * 3, 18 * 3, 22 * 3, 18 * 3, 18 * 3, 12 * 3],
+  [15 * 4, 22 * 4, 25 * 4, 22 * 4, 25 * 4, 17 * 4],
+  [20 * 5, 22 * 5, 28 * 5, 26 * 5, 30 * 5, 27 * 5]
 ]
 const replyRaw: Array<number[]> = [
   [0, 2, 3, 1, 1, 5],
